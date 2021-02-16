3cd26f6432688cb86559bd32fa5ad882c789d7cf
Marie Tritsch <marie.tsch@gmail.com>
2021-02-16 11:15:02 +0100

    routes v-1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :players, except: :destroy
+    resources :events, except: :destroy 
+    # do
+    #   resources :booking, only: [:index, :new, :create]
+    #   resources :canceling, only: [:destroy]
+    # end
+  end
+  resources :players, only: :destroy
+  resources :events, only: :destroy
 end
