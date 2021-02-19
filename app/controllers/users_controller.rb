6cf863b811010974a31a46bb07b606c6a112ce3c
Marie Tritsch <marie.tsch@gmail.com>
2021-02-19 05:15:55 +0100

    display correct event total price

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,14 @@ class UsersController < ApplicationController
     @player = Player.find(params[:id])
     @players = Player.where(user_id: @user)
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:success] = 'Bonne route, camarade...'
+    else
+      flash[:error] = 'Les adieux sont difficiles?'
+    end
+    redirect_to root_path
+  end
 end
